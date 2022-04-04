a24418541375cd5d6ecc6bb19ce619c51eb7dda1
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-04-04 13:54:54 +0600

    Update Algorithms.h

--- Cpp/Generic/Algorithms.h
+++ Cpp/Generic/Algorithms.h
@@ -7,6 +7,58 @@
 template <class U>
 class Algorithms{
 
+    private:
+        //INNER FUNCTIONALITIES
+    int partition(U arr[],int start,int end){
+        int i=start-1;
+        for(int j=start;j<end;j++){
+            if(arr[j]<arr[end]){
+                i++;
+                std::swap(arr[i],arr[j]);
+            }
+        }
+        i++;
+        std::swap(arr[i],arr[end]);
+        return i;
+    }
+    
+    void divide(U arr[],int start,int end){
+        if(start>=end){
+            return;
+        }
+        int mid = start+(end-start)/2;
+        divide(arr,start,mid);
+        divide(arr,mid+1,end);
+        conquer(arr,start,mid,end);
+    }
+
+    void conquer(U arr[],int start,int mid,int end){
+        int len = end-start+1;
+        U merged[len];
+        int x=0,index1=start,index2=mid+1;
+        while (index1<=mid && index2<=end)
+        {
+            if(arr[index1]<=arr[index2]){
+                merged[x++]=arr[index1++];
+            }else{
+                merged[x++]=arr[index2++];
+            }
+        }
+
+        while(index1<=mid){
+            merged[x++]=arr[index1++];
+        }
+
+        while(index2<=end){
+            merged[x++]=arr[index2++];
+        }
+
+        for(int i=0,j=start;i<(sizeof(merged)/sizeof(merged[0]));i++,j++){
+            arr[j]=merged[i];
+        }
+    }
+    
+
     public:
         //PUBLIC METHODS
     static int binarySearch(U arr[],int n,int elem){
@@ -69,9 +121,8 @@ class Algorithms{
     }
 
     static void quickSort(U arr[],int start,int end){
-        int pivot;
         if(start<end){
-            pivot=partition(arr,start,end);
+            int pivot=partition(arr,start,end);
             quickSort(arr,pivot+1,end);
             quickSort(arr,start,pivot-1);
         }
@@ -79,57 +130,6 @@ class Algorithms{
 
     static void mergeSort(U arr[],int start,int end){
         divide(arr,start,end);
-    }
-
-    private:
-        //INNER FUNCTIONALITIES
-    int partition(U arr[],int start,int end){
-        int i=start-1;
-        for(int j=start;j<end;j++){
-            if(arr[j]<arr[end]){
-                i++;
-                std::swap(arr[i],arr[j]);
-            }
-        }
-        i++;
-        std::swap(arr[i],arr[end]);
-        return i;
-    }
-
-    void conquer(U arr[],int start,int mid,int end){
-        int len = end-start+1;
-        U merged[len];
-        int x=0,index1=start,index2=mid+1;
-        while (index1<=mid && index2<=end)
-        {
-            if(arr[index1]<=arr[index2]){
-                merged[x++]=arr[index1++];
-            }else{
-                merged[x++]=arr[index2++];
-            }
-        }
-
-        while(index1<=mid){
-            merged[x++]=arr[index1++];
-        }
-
-        while(index2<=end){
-            merged[x++]=arr[index2++];
-        }
-
-        for(int i=0,j=start;i<(sizeof(merged)/sizeof(merged[0]));i++,j++){
-            arr[j]=merged[i];
-        }
-    }
-
-    void divide(U arr[],int start,int end){
-        if(start>=end){
-            return;
-        }
-        int mid = start+(end-start)/2;
-        divide(arr,start,mid);
-        divide(arr,mid+1,end);
-        conquer(arr,start,mid,end);
     }
 
 };
